e30535b1dbc2763ededa0a5b3c285c3e4d3c46ce
mete <metehanbucan@gmail.com>
2026-08-07 19:32:05 +0300

    feat: Add theme and signup page with custom input fields

---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,35 @@
+import 'package:blog_app/features/auth/presentation/widgets/auth_field.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            AuthField(hintText: 'name'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'email'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'password'),
+          ],
+        ),
+      ),
+    );
+  }
+}
