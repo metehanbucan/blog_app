137afd425b10ecbfb37c34ae8377a79acd5ff907
mete <metehanbucan@gmail.com>
2026-08-16 01:55:27 +0300

    feat: Implement user login functionality with BLoC integration and update dependencies

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:blog_app/features/auth/domain/entities/user.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,22 +11,39 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
-  AuthBloc({required UserSignUp userSignUp})
+  final UserLogin _userLogin;
+  AuthBloc({required UserSignUp userSignUp, required UserLogin userLogin})
     : _userSignUp = userSignUp,
+      _userLogin = userLogin,
       super(AuthInitial()) {
-    on<AuthSignUp>((event, emit) async {
-      emit(AuthLoading());
-      final res = await _userSignUp(
-        UserSignUpParams(
-          name: event.name,
-          email: event.email,
-          password: event.password,
-        ),
-      );
-      res.fold(
-        (l) => emit(AuthFailure(l.message)),
-        (user) => emit(AuthSuccess(user)),
-      );
-    });
+    on<AuthSignUp>(_onAuthSignUp);
+
+    on<AuthLogin>(_onAuthLogin);
+  }
+
+  void _onAuthSignUp(AuthSignUp event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final res = await _userSignUp(
+      UserSignUpParams(
+        name: event.name,
+        email: event.email,
+        password: event.password,
+      ),
+    );
+    res.fold(
+      (l) => emit(AuthFailure(l.message)),
+      (user) => emit(AuthSuccess(user)),
+    );
+  }
+
+  void _onAuthLogin(AuthLogin event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final res = await _userLogin(
+      UserLoginParams(email: event.email, password: event.password),
+    );
+    res.fold(
+      (l) => emit(AuthFailure(l.message)),
+      (user) => emit(AuthSuccess(user)),
+    );
   }
 }
--- lib/init_dependiences.dart
+++ lib/init_dependiences.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -31,7 +32,14 @@ void _initAuth() {
     () => UserSignUp(serviceLocator<AuthRepositoryImpl>()),
   );
 
+  serviceLocator.registerFactory(
+    () => UserLogin(serviceLocator<AuthRepositoryImpl>()),
+  );
+
   serviceLocator.registerLazySingleton(
-    () => AuthBloc(userSignUp: serviceLocator<UserSignUp>()),
+    () => AuthBloc(
+      userSignUp: serviceLocator<UserSignUp>(),
+      userLogin: serviceLocator<UserLogin>(),
+    ),
   );
 }
