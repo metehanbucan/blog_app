4d0bdb583601a5a0a0710beb41b53711ae60084c
mete <metehanbucan@gmail.com>
2026-08-18 20:55:47 +0300

    feat: Add functionality to fetch all blogs with BLoC integration and update UI accordingly

--- lib/init_dependiences.dart
+++ lib/init_dependiences.dart
@@ -8,6 +8,7 @@ import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
 import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
+import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -59,5 +60,9 @@ void _initBlog() {
       () => BlogRepositoryImpl(serviceLocator()),
     )
     ..registerFactory<UploadBlog>(() => UploadBlog(serviceLocator()))
-    ..registerLazySingleton(() => BlogBloc(serviceLocator()));
+    ..registerFactory<GetAllBlogs>(() => GetAllBlogs(serviceLocator()))
+    ..registerLazySingleton(
+      () =>
+          BlogBloc(uploadBlog: serviceLocator(), getAllBlogs: serviceLocator()),
+    );
 }
