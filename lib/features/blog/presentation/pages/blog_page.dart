a0c6f3c5e1fe6e40e05830c52afae4a888b06378
mete <metehanbucan@gmail.com>
2026-08-19 18:22:34 +0300

    feat: Add reading time calculation to BlogCard and update UI for dynamic margin

--- lib/features/blog/presentation/pages/blog_page.dart
+++ lib/features/blog/presentation/pages/blog_page.dart
@@ -52,7 +52,14 @@ class _BlogPageState extends State<BlogPage> {
               itemCount: state.blogs.length,
               itemBuilder: (context, index) {
                 final blog = state.blogs[index];
-                return BlogCard(blog: blog, color: AppPallete.gradient1);
+                return BlogCard(
+                  blog: blog,
+                  color: index % 3 == 0
+                      ? AppPallete.gradient1
+                      : index % 3 == 1
+                      ? AppPallete.gradient2
+                      : AppPallete.gradient3,
+                );
               },
             );
           }
